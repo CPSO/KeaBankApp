b702b76a5bd3032bf313fc314d575b8eb91e85f1
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2019-05-02 23:16:42 +0200

    save point

---
+++ app/src/main/java/com/example/keabankapp/models/AccountModel.java
@@ -0,0 +1,14 @@
+package com.example.keabankapp.models;
+
+public class AccountModel {
+    private String aName;
+    private int aAmout;
+
+    public AccountModel(String aName, int aAmout) {
+        this.aName = aName;
+        this.aAmout = aAmout;
+    }
+
+    public AccountModel() {
+    }
+}
