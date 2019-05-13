e811fd436941489ed4ca82eccef75f68d070d43f
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2019-05-13 16:29:11 +0200

    bug on submit

--- app/src/main/java/com/example/keabankapp/account/PopActivity.java
+++ app/src/main/java/com/example/keabankapp/account/PopActivity.java
@@ -1,20 +1,33 @@
 package com.example.keabankapp.account;
 
 import android.app.Activity;
+import android.icu.util.ValueIterator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.keabankapp.R;
 
-public class PopActivity extends Activity {
 
+
+public class PopActivity extends Activity {
+    Button btnSubmit;
+    EditText etAmount;
+
+    private static final String TAG = "PopActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
+        btnSubmit = findViewById(R.id.btnDepositConfirm);
+        etAmount = findViewById(R.id.etDepositAmount);
+        btnSubmit.setOnClickListener(OnClickListenerSubmit);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -31,4 +44,15 @@ public class PopActivity extends Activity {
 
         getWindow().setAttributes(params);
     }
+    private View.OnClickListener OnClickListenerSubmit = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            double amount = Double.parseDouble(String.valueOf(etAmount.getText()));
+
+            Log.d(TAG, "onClick: ");
+            Log.d(TAG, "onClick: " + amount);
+
+
+        }
+    };
 }
